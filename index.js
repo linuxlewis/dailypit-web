697c06f96cac48e9b40ca391222492edef4c5446
Sam Bolgert <sbolgert@gmail.com>
2015-04-05 02:13:11 -0700

    0.0.6 Release

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
+app.use(require('prerender-node').set('prerenderToken', 'nuWyXXOkCS52aGgfD9TQ'));
 
 app.listen(process.env.PORT || 3000);
